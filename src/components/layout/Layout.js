e3ecdfa4f6d4b0c1315f4ae51dc595e4e1aa666d
chopsuey123 <merthzl98@gmail.com>
2023-05-28 23:51:04 +0300

    Quote Project With React Router Dom

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,14 @@
+import MainNavigation from "./MainNavigation";
+import React from "react";
+import classes from "./Layout.module.css"
+
+const Layout = (props) => {
+  return (
+    <React.Fragment>
+      <MainNavigation />
+      <main className={classes.main}>{props.children}</main>
+    </React.Fragment>
+  );
+};
+
+export default Layout;
---
+++ src/pages/NotFound.js
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <div className="centered">Not found page! 404</div>;
+};
+
+export default NotFound;
